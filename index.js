ef90e46853516b4d2a058669d7e4abb0792f645e
GAMEBOOK <zazouuke@gmail.com>
2018-05-06 19:31:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const Discord = require('discord.js')
+const bot = new Discord.Client()
 
-// Set the prefix
-let prefix = "!";
-client.on("message", (message) => {
-  // Exit and stop if the prefix is not there or if user is a bot
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
+bot.on('ready', function () {
+  console.log("Je suis connecté !")
+})
 
-  if (message.content.startsWith(prefix + "ping")) {
-    message.channel.send("pong!");
-  } else
-  if (message.content.startsWith(prefix + "foo")) {
-    message.channel.send("bar!");
-  }
-});
-
-client.login("u3n6aYB0erda2JO47bpPHCG8cKo6eo2e");
+bot.login('u3n6aYB0erda2JO47bpPHCG8cKo6eo2e')
